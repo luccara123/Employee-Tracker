bae1b74d4d799c884d4414ca9f9c317f8af0fd8e
Lucca Araujo <lucca.ara7@gmail.com>
2022-05-14 19:31:37 -0700

    Added the prompt for the questions with a then response

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const {user, password} = require("./.env");
 require("dotenv").config();
+const inquirer = require("inquirer");
+const db = require("./db");
 
-const db = mysql.createConnection(
+// Use .env
+const connection = mysql.createConnection(
     {
         host: "localhost",
         user: process.env.USER,
@@ -10,3 +13,51 @@ const db = mysql.createConnection(
     },
     console.log("Connected to the employees database")
 )
+
+// Prompt the questions
+const FirstQuestions= () => {
+    inquirer
+    .prompt({
+      name: "menuOptions",
+      type: "list",
+      message: "Please choose an option.",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role"
+      ],
+      //a function will be called after an option is selected
+    }).then(function(response) {
+        switch (response.menuOptions) {
+        case "View all departments":
+            viewDepartments();
+            break;
+        case "View all roles":
+            viewRoles();
+            break;
+        case "View all employees":
+            viewEmployees();
+            break;
+          case "Add a department":
+            addDepartment();
+            break;
+          case "Add a role":
+            addRole();
+            break;
+          case "Add an employee":
+            addEmployee();
+            break;
+          case "Update an employee role":
+            updateEmployee();
+            break;
+          default:
+            exit();
+        }
+      });
+  }
+
+FirstQuestions();
